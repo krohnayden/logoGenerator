100cd50cc1b1c04f04e86e4d25759815dbcd31bb
Ayden Krohn <krohnayden@gmail.com>
2023-11-26 20:38:30 -0600

    fixed bugs in circle class and deleted unnecessary code

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function writeToFile(fileName, userInput) {
     let svg = "";
     svg = '<svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">';
     svg += "<g>";
-    svg += `${userInput.shapes}`
 
     let userShape;
     if (userInput.shapes === "Circle") {
